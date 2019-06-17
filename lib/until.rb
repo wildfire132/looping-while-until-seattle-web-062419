c4323edc6596d1eeece77d7fb39abc8dc9ae56b2
Katana Tran <tkatyk21@gmail.com>
2019-06-17 05:01:23 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
   levitation_force = 6
-  puts "Wingardium Leviosa"
-  levitation_force += 1
+  if levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end 
 end
 
